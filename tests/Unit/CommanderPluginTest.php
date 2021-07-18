09b3f19bb7c1ceb364887ade59882e121f7575e7
Eder Soares <edersoares@me.com>
2021-07-18 15:48:16 -0300

    Test run command from composer.json

--- tests/Unit/CommanderPluginTest.php
+++ tests/Unit/CommanderPluginTest.php
@@ -6,17 +6,41 @@ use Dex\Composer\Commander\Tests\Application;
 use Dex\Composer\Commander\Tests\TestCase;
 use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Console\Output\Output;
 
 class CommanderPluginTest extends TestCase
 {
+    private string $directory = __DIR__ . '/../Fixtures/Plugin';
     public function test()
     {
         $application = new Application();
-        $input = new StringInput("commands");
+        $input = new StringInput('commands');
         $output = new BufferedOutput();
 
         $application->doRun($input, $output);
 
         $this->assertStringContainsString('There are no custom commands', $output->fetch());
+    }
+
+    public function testCommandsInComposerJson()
+    {
+        $application = new Application();
+        $input = new StringInput('success -d ' . $this->directory);
+        $output = new BufferedOutput();
+
+        $application->doRun($input, $output);
+
+        $this->assertStringContainsString('Success', $output->fetch());
+    }
+
+    public function testCommandsInCommanderJson()
+    {
+        $application = new Application();
+        $input = new StringInput('success:commander -d ' . $this->directory);
+        $output = new BufferedOutput();
+
+        $application->doRun($input, $output);
+
+        $this->assertStringContainsString('Running commands from commander.json', $output->fetch());
     }
 }
